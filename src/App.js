85a74798b0950f4c2981a41293aa4a089978a56d
Alfredo Avalos <aavalos@gmail.com>
2017-10-18 14:44:53 -0600

    libraries and initial firebase setup

--- src/App.js
+++ src/App.js
@@ -11,17 +11,34 @@ import {
   Text,
   View
 } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+    'Cmd+D or shake for dev menu...',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
+componentWillMount() {
+  const config = {
+    apiKey: 'AIzaSyCvMr3JrSL7NOVr182dWuVQpHH9VZBSXaQ',
+    authDomain: 'cr100-27776.firebaseapp.com',
+    databaseURL: 'https://cr100-27776.firebaseio.com',
+    projectId: 'cr100-27776',
+    storageBucket: 'cr100-27776.appspot.com',
+    messagingSenderId: '590492106754'
+  };
+  firebase.initializeApp(config);
+}
+
   render() {
     return (
+      <Provider store={createStore(reducers)}>
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -33,6 +50,7 @@ export default class App extends Component<{}> {
           {instructions}
         </Text>
       </View>
+      </Provider>
     );
   }
 }
